53fcb007f79050d67853aca1d314039c16d1c530
Yuri Furquim <yuripatrick347@gmail.com>
2021-05-24 10:26:51 -0300

    Configured project and created ClienteController and ClienteModel

---
+++ src/main/java/com/sistema/entrega/api/SistemaEntragaMercadoriaApiApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.entrega.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEntragaMercadoriaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEntragaMercadoriaApiApplication.class, args);
+	}
+
+}
